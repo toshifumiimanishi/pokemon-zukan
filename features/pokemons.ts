9231a2ee954b1e0a246ad9328aa70d1f86143176
Toshifumi Imanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2021-10-17 15:55:09 +0900

    :sparkles: feat: Slice の新規作成

---
+++ features/pokemons.ts
@@ -0,0 +1,19 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Pokemons } from '../types';
+
+export type PokemonsState = { pokemons: Pokemons };
+
+const initialState: PokemonsState = {
+    pokemons: [],
+};
+
+export const pokemonsSlice = createSlice({
+    name: 'pokemons',
+    initialState,
+    reducers: {
+      pokemonsGotten: (state, action: PayloadAction<PokemonsState>) => ({
+            ...state,
+            pokemons: action.payload.pokemons,
+        }),
+    },
+});
