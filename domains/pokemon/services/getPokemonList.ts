3f7ad1bc20953b9081cf8ce20f36ac336fdccc79
Toshifumi Imanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2021-10-21 09:34:49 +0900

    :+1: feat: PokéAPI レスポンスを返す関数へ改修

--- domains/pokemon/services/getPokemonList.ts
+++ domains/pokemon/services/getPokemonList.ts
@@ -1,3 +1,9 @@
 import { apiClient } from './apiClient';
+import { PokemonsGotten } from '../../../types';
 
-export const getPokemonList = () => apiClient.get('pokemon');
+export const getPokemons = async () => {
+  const response = await apiClient.get<PokemonsGotten>('pokemon');
+  const pokemons = response.data.results;
+
+  return pokemons;
+};
