022d3059ab0cefbd04d4c89b568fc69be1002888
Toshifumi Imanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2021-10-24 10:56:19 +0900

    :+1: feat: aspida 仕様へ API クラインアントの改修

--- domains/pokemon/services/apiClient.ts
+++ domains/pokemon/services/apiClient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
+import aspida from '@aspida/axios';
+import api from '../../../api/$api';
 
-export const apiClient = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2',
-});
+const baseURL = 'https://pokeapi.co/api';
+export const apiClient = api(aspida(axios, { baseURL }));
--- domains/pokemon/services/getPokemonList.ts
+++ domains/pokemon/services/getPokemonList.ts
@@ -1,9 +1,8 @@
 import { apiClient } from './apiClient';
-import { PokemonsGotten } from '../../../types';
 
 export const getPokemons = async () => {
-  const response = await apiClient.get<PokemonsGotten>('pokemon');
-  const pokemons = response.data.results;
+  const response = await apiClient.v2.pokemon.get();
+  const pokemons = response.body.results;
 
   return pokemons;
 };
