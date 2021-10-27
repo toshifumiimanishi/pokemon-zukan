36a5fb87afe11aead406584d3df0b34ef6890cd9
Toshifumi Imanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2021-10-27 12:49:14 +0900

    :jigsaw: feat: エンドポイント型定義ファイルの改修

--- api/$api.ts
+++ api/$api.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 // prettier-ignore
-import { AspidaClient } from 'aspida'
+import { AspidaClient, dataToURLString } from 'aspida'
 // prettier-ignore
 import { Methods as Methods0 } from './v2/pokemon'
 
@@ -13,11 +13,12 @@ const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
   return {
     v2: {
       pokemon: {
-        get: (option?: { config?: T }) =>
+        get: (option?: { query?: Methods0['get']['query'], config?: T }) =>
           fetch<Methods0['get']['resBody']>(prefix, PATH0, GET, option).json(),
-        $get: (option?: { config?: T }) =>
+        $get: (option?: { query?: Methods0['get']['query'], config?: T }) =>
           fetch<Methods0['get']['resBody']>(prefix, PATH0, GET, option).json().then(r => r.body),
-        $path: () => `${prefix}${PATH0}`
+        $path: (option?: { method?: 'get'; query: Methods0['get']['query'] }) =>
+          `${prefix}${PATH0}${option && option.query ? `?${dataToURLString(option.query)}` : ''}`
       }
     }
   }
--- api/v2/pokemon/index.ts
+++ api/v2/pokemon/index.ts
@@ -2,6 +2,10 @@ import { PokemonsGotten } from '../../../types';
 
 export type Methods = {
   get: {
+    query?: {
+      offset: number
+      limit: number
+    }
     resBody: PokemonsGotten;
   };
 };
